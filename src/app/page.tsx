38ae2c504c2ac55a1a179f27911fc781f025db36
Art Perch <art.perchatkin@gmail.com>
2024-12-07 17:15:14 +0500

    add YMap iframe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
+'use client';
 import AboutBlock from "./Blocks/AboutBlock/AboutBlock";
 import Slider from "./Blocks/Slider/Slider";
-import Location from "./Blocks/Location/Location";
 import Form from "./Blocks/Form/Form";
 import PartnersBlock from "./Blocks/PartnersBlock/PartnersBlock";
 import IntroBlock from "./Blocks/introBlock/IntroBlock";
 import { IntroBlockBg } from "./imgs/imgIndex/imgIndex";
 import Footer from "./components/footer/Footer";
+import dynamic from 'next/dynamic';
+
+const Location = dynamic(() => import('@/app/Blocks/Location/Location'), { ssr: false });
 
 export default function Home() {
   return (
